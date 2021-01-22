a96a8e06d37847b7a1ff8d21f2da7bd22c316b2a
Jack Li(Analytics Engineering) <jlli@jlli-mn1.linkedin.biz>
2021-01-22 13:04:21 -0800

    Support data ingestion for offline segment in one pass

--- pinot-core/src/main/java/org/apache/pinot/core/indexsegment/generator/SegmentGeneratorConfig.java
+++ pinot-core/src/main/java/org/apache/pinot/core/indexsegment/generator/SegmentGeneratorConfig.java
@@ -105,6 +105,7 @@ public class SegmentGeneratorConfig implements Serializable {
   private boolean _onHeap = false;
   private boolean _skipTimeValueCheck = false;
   private boolean _nullHandlingEnabled = false;
+  private boolean _isIntermediateSegmentRecordReader = false;
 
   // constructed from FieldConfig
   private Map<String, Map<String, String>> _columnProperties = new HashMap<>();
@@ -669,5 +670,13 @@ public class SegmentGeneratorConfig implements Serializable {
 
   public void setNullHandlingEnabled(boolean nullHandlingEnabled) {
     _nullHandlingEnabled = nullHandlingEnabled;
+  }
+
+  public boolean isIntermediateSegmentRecordReader() {
+    return _isIntermediateSegmentRecordReader;
+  }
+
+  public void setIntermediateSegmentRecordReader(boolean intermediateSegmentRecordReader) {
+    _isIntermediateSegmentRecordReader = intermediateSegmentRecordReader;
   }
 }
